0d1a6b80596e4d3a0a1a35a6c77bf9afa5b67926
Carter Walker <carter@goldtone.com>
2024-11-17 11:50:44 -0500

    Evolving to std::io!

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
-// Learning Rust!
+// This looks familiar...
+use std::io;
+
+// I missed compiled languages so bad.
+// Define that main function, baby!
 fn main() {
-    println!("Hello, world!");
+
+    println!("Guess the number!");
+
+    println!("Please input your guess.");
+
+    // Must define as mutable,
+    // also hard-cast as empty?
+    let mut guess  = String::new();
+
+    // Use the aforementioned deep sea texts
+    io::stdin()
+        .read_line(&mut guess)
+        // Always use error hadling.
+        // Under the sea safety 101.
+        .expect("Failed to read line");
+
+    println!("You guessed: {}", guess);
 }
